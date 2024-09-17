5127f0b69944bf55615c4b0f3974cb024737060c
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-18 01:09:52 +0400

    finish task 3

---
+++ MyPlayground.playground/Pages/task3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+//3. შექმენით ორი Double ტიპის ცვლადი და გამოიყენეთ შედარების ოპერატორები (==, !=, >, <, >=, <=) მათ შესადარებლად. დაბეჭდეთ შედარების შედეგები.
+
+import Foundation
+
+let num1: Double = 18.18
+let num2: Double = 0.33
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+//: [Next](@next)
