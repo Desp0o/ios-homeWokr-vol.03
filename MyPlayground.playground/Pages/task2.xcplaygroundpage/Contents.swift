ed209d44d71c0d10fb073546feae752bc4c81d72
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-18 01:05:21 +0400

    finish task 2

---
+++ MyPlayground.playground/Pages/task2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+//: [Previous](@previous)
+
+//2. შექმენით Int ტიპის ცვლადი და გამოიყენეთ არითმეტიკული ოპერატორები (+, -, *, /) სხვადასხვა ოპერაციების შესასრულებლად. დაბეჭდეთ შედეგები.
+
+import Foundation
+
+var number = 99
+
+number += 10
+print(number)
+
+number -= 10
+print(number)
+
+number *= 7
+print(number)
+
+let remains = number % 2
+print(remains)
+
+number /= 77
+print(number)
+
+
+
+//: [Next](@next)
