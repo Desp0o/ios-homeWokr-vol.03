5e5c5b92c80ca7bac97302b1ce3b74c657058f73
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-18 01:12:44 +0400

    finish task 4

---
+++ MyPlayground.playground/Pages/task4.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+//4. შექმენით String ტიპის ცვლადი თქვენი სახელით და Character ტიპის მუდმივა თქვენი სახელის პირველი ასოთი. გამოიყენეთ სტრინგის ინტერპოლაცია ორივე მნიშვნელობის დასაბეჭდად ერთ წინადადებაში.
+
+import Foundation
+
+let myName = "Tornike"
+let firstLetter: Character = "T"
+let mergedString = "\(firstLetter) for \(myName)"
+
+print(mergedString)
+
+
+//: [Next](@next)
