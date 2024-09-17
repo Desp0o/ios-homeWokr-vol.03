22f157200ecfc2bb09d95db1760efba02e5a8f77
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-18 01:51:49 +0400

    finish task 8

---
+++ MyPlayground.playground/Pages/task8.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+//8. დაწერეთ if-else if-else სტრუქტურა, რომელიც შეამოწმებს Int ტიპის ცვლადის მნიშვნელობას და დაბეჭდავს შესაბამის შეტყობინებას (მაგალითად, დადებითია, უარყოფითია თუ ნულის ტოლი).
+
+import Foundation
+
+let num = 9
+
+if num == 0 {
+    print("\(num) is zero")
+} else if num % 2 == 0 {
+    print("\(num) is even")
+} else {
+    print("\(num) is odd")
+}
+
+//: [Next](@next)
