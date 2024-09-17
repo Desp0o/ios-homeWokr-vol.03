09871271e6a92ed23a9b3185d2352e367e5fa489
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-18 02:31:27 +0400

    finish task 11

---
+++ MyPlayground.playground/Pages/task11.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+//11. შექმენით სტრინგ ტიპის ცვლადი და მიანიჭეთ მნიშვნელობა, დაბეჭდეთ მისი შებრუნებული მნიშვნელობა, მაგ: “xCode” -> “”edoCx”
+
+
+import Foundation
+
+let word = "Aloha"
+let reversedWord = word.split(separator: "").reversed().joined(separator: "")
+
+print("\(word) -> \(reversedWord)")
+
+//: [Next](@next)
