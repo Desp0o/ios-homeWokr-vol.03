deaea6acf7766b6745c274ff00750b1221048e36
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-18 01:18:40 +0400

    finish task 5

---
+++ MyPlayground.playground/Pages/task5.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+//5. შექმენით Optional String ტიპის ცვლადი. გამოიყენეთ optional binding (if let), რომ უსაფრთხოდ ამოიღოთ მნიშვნელობა და დაბეჭდოთ ის.
+
+
+import Foundation
+
+var greeting: String?
+
+if let temp = greeting {
+    print(temp)
+}else{
+    print("There is no string")
+}
+
+//: [Next](@next)
