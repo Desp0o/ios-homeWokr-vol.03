b6c517cc6ef18b81fc051f367fcbe54c7edcdbb9
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-18 02:24:03 +0400

    finish task 10

---
+++ MyPlayground.playground/Pages/task10.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: [Previous](@previous)
+
+//10. შექმენით String ტიპის ცვლადი სიტყვით და Int ტიპის ცვლადი სახელით count ნულის მნიშვნელობით. გამოიყენეთ for-in ციკლი სიტყვაში ხმოვნების (a, e, i, o, u) დასათვლელად. ყოველი ხმოვნის შემთხვევაში გაზარდეთ count ერთით. ციკლის დასრულების შემდეგ დაბეჭდეთ საწყისი სიტყვა და მასში ნაპოვნი ხმოვნების რაოდენობა.
+
+
+import Foundation
+
+let word = "Foundation"
+let targetLetters = "aeiou"
+var count = 0
+
+for letter in word {
+    for targetedLetter in targetLetters{
+        if(targetedLetter == letter){
+            count += 1
+        }
+    }
+}
+
+print("there is \(count) vowel letter in word \(word)")
+
+print("-------------------")
+
+let word2 = "pneumonoultramicroscopicsilicovolcanoconiosis"
+let targetLetters2 = "aeiou"
+var count2 = 0
+
+for letter in word2{
+    if targetLetters2.contains(letter){
+        count2 += 1
+    }
+}
+
+print("there is \(count2) vowel letter in word2 \(word2)")
+
+
+//: [Next](@next)
---
+++ MyPlayground.playground/Pages/task9.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+//9. შექმენით ორი Int ტიპის ცვლადი: number და sum. გამოიყენეთ while ციკლი number-ის ციფრების ჯამის გამოსათვლელად და შედეგი შეინახეთ sum-ში. მაგალითად, თუ number არის 1234, sum უნდა იყოს 10 (1+2+3+4). დაბეჭდეთ საწყისი რიცხვი და მიღებული ჯამი.
+
+import Foundation
+
+var number = 1234
+
+var test = number.words.split(separator: "")
+var greeting = "Hello, playground"
+
+//: [Next](@next)
