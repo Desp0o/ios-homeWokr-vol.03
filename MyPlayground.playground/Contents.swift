8b351c9ee298dea6cb87c8e44c2787c7ec1e2647
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-18 00:11:27 +0400

    finish task 1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,14 @@
+//1. შექმენით ორი მუდმივა (let) და ორი ცვლადი (var) სხვადასხვა ტიპის (Int, Double, String, Bool). მიანიჭეთ მათ მნიშვნელობები და დაბეჭდეთ.
+
 import UIKit
 
-var greeting = "Hello, playground"
+let name = "Tornike"
+let isLearning = true
+var age = 30
+var weight = 70.200
+
+print(name)
+print(isLearning)
+print(age)
+print(weight)
+
