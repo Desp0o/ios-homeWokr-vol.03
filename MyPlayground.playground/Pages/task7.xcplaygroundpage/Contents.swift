f92d23454539d689c75d1abf9360829f074e7be6
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-18 01:38:32 +0400

    finish task 7

---
+++ MyPlayground.playground/Pages/task7.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+//7. შექმენით Int ტიპის ცვლადი და გამოიყენეთ while ციკლი, რომ დაბეჭდოთ მისი მნიშვნელობა 0-მდე (ყოველ იტერაციაზე აკლებთ 1-ს).
+
+
+import Foundation
+
+var number = 10
+
+while number >= 0 {
+    print(number)
+    number -= 1
+}
+
+//: [Next](@next)
