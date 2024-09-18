01d427b036cf50950ddbb5cac9dce73b09c7b5b6
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-18 10:27:38 +0400

    finish task 12

--- MyPlayground.playground/Pages/task12.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/task12.xcplaygroundpage/Contents.swift
@@ -1,7 +1,19 @@
 //: [Previous](@previous)
 
+//12. შქმენით Int ტიპის ცვლადი და მიანიჭეთ მას ორნიშნა მნიშვნელობა, შეამოწმეთ შედგება თუ არა ეს რიცხვი ერთი და იგივე ციფრებისგან.
+
+
 import Foundation
 
-var greeting = "Hello, playground"
+let number = 11;
+let secondNum = number % 10;
+let firstNum = number / 10;
+
+if firstNum == secondNum {
+    print("first number \(firstNum) and second number \(secondNum) are equals")
+}else{
+    print("first number \(firstNum) and second number \(secondNum) are not equals")
+}
+
 
 //: [Next](@next)
