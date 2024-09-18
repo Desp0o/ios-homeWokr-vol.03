32bba2a8abf3aa6f9b9b77a61ac1f3b0a657cc76
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-18 10:30:05 +0400

    finish task 9

--- MyPlayground.playground/Pages/task9.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/task9.xcplaygroundpage/Contents.swift
@@ -1,18 +1,19 @@
-////: [Previous](@previous)
-//
-////9. შექმენით ორი Int ტიპის ცვლადი: number და sum. გამოიყენეთ while ციკლი number-ის ციფრების ჯამის გამოსათვლელად და შედეგი შეინახეთ sum-ში. მაგალითად, თუ number არის 1234, sum უნდა იყოს 10 (1+2+3+4). დაბეჭდეთ საწყისი რიცხვი და მიღებული ჯამი.
-//
-//import Foundation
-//
-//let number = 1234
-//var sum = 0
-//var temp = number
-//
-//while temp > 0 {
-//    let lastNum = number % 10
-//    sum += lastNum
-//    temp /= 10
-//}
-////: [Next](@next)
-//
-//print(sum)
+//: [Previous](@previous)
+
+//9. შექმენით ორი Int ტიპის ცვლადი: number და sum. გამოიყენეთ while ციკლი number-ის ციფრების ჯამის გამოსათვლელად და შედეგი შეინახეთ sum-ში. მაგალითად, თუ number არის 1234, sum უნდა იყოს 10 (1+2+3+4). დაბეჭდეთ საწყისი რიცხვი და მიღებული ჯამი.
+
+import Foundation
+
+let number = 1234
+var sum = 0
+var temp = number
+
+while temp > 0 {
+    var lastNum = temp % 10
+    sum += lastNum
+    temp /= 10
+    print(lastNum)
+}
+//: [Next](@next)
+
+print(sum)
