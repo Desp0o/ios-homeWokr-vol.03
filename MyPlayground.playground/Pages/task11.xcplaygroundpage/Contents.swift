5684b00e49b4dcb9d5468c5f98a5e95c438a02b7
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-19 11:13:53 +0400

    use for loop for reverse word

--- MyPlayground.playground/Pages/task11.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/task11.xcplaygroundpage/Contents.swift
@@ -6,8 +6,21 @@
 import Foundation
 
 var word = "Aloha"
-let reversedWord = word.split(separator: "").reversed().joined(separator: "")
+var reversedWord = word.split(separator: "").reversed().joined(separator: "")
 
 print("\(word) -> \(reversedWord)")
+
+
+print("----------")
+
+var word2 = "Hippopotamus"
+var reversedWord2 = ""
+
+for letter in stride(from: word2.count - 1, through: 0, by: -1) {
+    let letterIndex = word2.index(word2.startIndex, offsetBy: letter)
+    reversedWord2 += String(word2[letterIndex])
+}
+
+print(reversedWord2)
 
 //: [Next](@next)
